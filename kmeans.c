8fce1b0bbe300558e1a8fddbf89272b36af59af6
spitzerhadar <hadar.spitzer@live.biu.ac.il>
2023-05-18 15:55:42 +0300

    .

--- kmeans.c
+++ kmeans.c
@@ -19,6 +19,33 @@ int countElements(LINK);
 LINK createList(void);
 void printMat(double**);
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+typedef struct Centroid
+{
+    double* selfVector;
+    int numOfVectors;
+    double** relatedVectors;
+}Centroid;
+
+double** deepCopy2DArray(double** inputArray, int rows, int columns);
+double* copyArray(double* inputArray, int rows);
+double update(Centroid* centroid, int vectorsLength);
+Centroid* calcClosestCentroid(double *vector, Centroid **centroids, int K, int vectorsLength);
+double euclidianDistance(double *vector1, double *vector2, int vectorsLength);
+double* copyArray(double* inputArray, int rows);
+void zeroArray(double* array, int arrayLength);
+double averageOf(Centroid* centroid, int i);
+void freeRelatedVectors(Centroid* centroid);
+double maxDelta(double *deltas, int numberOfVectors);
+double** getCentroidsSelfVectors(Centroid* centroids, int K);
+int countDigitsOfWholePart(double value);
+
 int getVectorSize(char* str){
     int i;
     int size = 1;
