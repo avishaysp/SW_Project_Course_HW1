2105e912dfbf22bbfe9faad6f7c4401e5b336c24
spitzerhadar <hadar.spitzer@live.biu.ac.il>
2023-05-18 20:52:11 +0300

    .

--- kmeans.c
+++ kmeans.c
@@ -83,7 +83,9 @@ struct inputMat createMatrix(void){
         for(j = 0; j < vectorsLength; j++){
             printf("%d\n", 4);
             mat[i][j] = c.value;
-            c = *c.next;
+            if (j < vectorsLength - 1) {
+                c = *c.next;
+            }
             printf("%d\n", 5);
         }
         printf("%d\n", 6);
